1c124cd1b9cc736df452742d0efc2bfe2008d95f
elpacho27 <patrik27ocelic03@gmail.com>
2026-03-17 20:03:46 +0100

    AllowCredentials added

--- Backend/ROFit/Program.cs
+++ Backend/ROFit/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 var firebaseJson = Environment.GetEnvironmentVariable("FIREBASE_CREDENTIALS");
